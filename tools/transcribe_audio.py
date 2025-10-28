fab6b7b522b99b7048700162594429171e81215c
Michael <mjs070594@gmail.com>
2025-10-28 10:01:01 -0500

    created log paths and writing transcription logic

--- tools/transcribe_audio.py
+++ tools/transcribe_audio.py
@@ -36,4 +36,25 @@ def transcribe_audio(file_path: str, summarize: bool = False): #since summary is
     txt_path = os.path.join(output_dir, f"transcribed_{base_name}_{timestamp}.txt")
     summary_path = os.path.join(output_dir, f"ai_summary_{base_name}_{timestamp}.txt")
 
-    print(f"\n🎙️ Transcribing: {file_path}\n(This may take a few minutes for long files...)")
+    print(f"\n ✍️ Transcribing: {file_path}\n(This may take a few minutes for larger files...)")
+
+    # Perform transcription - feeding audio file to the model
+    segments, info = model.transcribe(file_path, beam_size=5)
+
+    """
+    model.transcribe() streams the audio through the Faster-Whisper model.
+    beam_size=5 → controls accuracy (5 beams = slightly slower but better results)
+        
+        The function returns:
+            segments → list of timestamped text chunks.
+            info → metadata (i.e language, duration, etc.)
+    """
+
+    # Write transcription to file
+    with open(txt_path, "w", encoding="utf-8") as f: #context manager to handle file operations - "w" = in write mode with utf-8 encoding
+        f.write(f"Transcription ({file_path})\n")
+        f.write(f"Language: {info.language}\n\n") #write language info from metadata
+        for segment in segments: #loop through each segment with timestamps
+            f.write(f"[{segment.start:.2f}s → {segment.end:.2f}s] {segment.text}\n")
+
+    print(f"\n✅ Transcription complete! Saved to:\n{txt_path}")
